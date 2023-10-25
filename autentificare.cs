783a697bd2f7b883470db08bf87e8c6c14ef88af
timoteifrincuu <146390788+timoteifrincuu@users.noreply.github.com>
2023-10-25 23:46:10 +0300

    Add files via upload

---
+++ autentificare.cs
@@ -0,0 +1,25 @@
+﻿const string UtilizatorCorect = "utilizator";
+const string ParolaCorecta = "parola123";
+
+bool autentificareReusita = false;
+
+while (!autentificareReusita)
+{
+    Console.Write("Introduceți numele de utilizator: ");
+    string utilizator = Console.ReadLine();
+
+    Console.Write("Introduceți parola: ");
+    string parola = Console.ReadLine();
+
+    if (utilizator == UtilizatorCorect && parola == ParolaCorecta)
+    {
+        autentificareReusita = true;
+        Console.WriteLine("Bine ai venit, " + utilizator + "!");
+    }
+    else
+    {
+        Console.WriteLine("Autentificare eșuată. Vă rugăm să încercați din nou.");
+    }
+}
+
+Console.WriteLine("Acum sunteți autentificat în aplicație.");
